4003b8564ea98d9ce4548d71caee7b87d8c8f245
DESKTOP-C1T53C2\Lenovo <limaijie1011@sina.com>
2020-04-22 18:51:56 +0800

    vue3.0+»ù´¡ÅäÖô´½¨

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+    <h1>从这里开始创建Vue项目</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
