63c8a9a6e0537d503102d1aa19b7b3c9bc8658ff
wdb111 <1303329454@qq.com>
2020-08-19 11:04:47 +0800

    router文件修改

--- src/router/index.js
+++ src/router/index.js
@@ -6,11 +6,20 @@ Vue.use(Router)
 
 const router = new Router({
     routes: [{
-            path: '/',
-            name: "HelloWorld",
-            component: HelloWorld,
-        }
-    ]
+        path: '/',
+        name: "HelloWorld",
+        component: HelloWorld,
+    }]
 })
 
+//路由跳转前做点什么
+router.beforeEach((to, from, next) => {
+    // console.log(to)
+    next()
+});
+
+//路由跳转后做点什么
+router.afterEach(to => {
+    // window.scrollTo(0, 0)
+});
 export default router;
